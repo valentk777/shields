7d79b1d6bdce263204b47435e7473ce749c98479
Paul Melnikow <email@paulmelnikow.com>
2020-10-21 13:42:54 -0400

    [DEPLOY] Completely clean the index

--- services/criterion/constants.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = Object.freeze({
-  IMPROVED_STATUS: 'improved',
-  REGRESSED_STATUS: 'regressed',
-  NO_CHANGE_STATUS: 'no change',
-  NOT_FOUND_STATUS: 'no status found',
-})
